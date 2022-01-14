93b3583e9c60d8816b434e223ddfe7070a4ec39e
Aaron Zhang <59893492+aaronz1999@users.noreply.github.com>
2022-01-14 03:35:47 -0700

    Update lab1.py

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -6,5 +6,5 @@ homePage = requests.get('http://www.google.com')
 print(homePage)
 
 print('\n Github')
-githubCode = requests.get('https://raw.githubusercontent.com/aaronz1999/CMPUT404/lab1/main/lab1.py')
+githubCode = requests.get('https://raw.githubusercontent.com/aaronz1999/CMPUT404/main/lab1/lab1.py')
 print(githubCode.text)
